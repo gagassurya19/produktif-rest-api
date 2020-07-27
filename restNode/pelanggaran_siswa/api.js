a7190874cd9d838c7db5857661b12b2d89369c74
Gagassurya19 <55011173+gagassurya19@users.noreply.github.com>
2020-07-27 10:31:31 +0700

    move dir

---
+++ restNode/pelanggaran_siswa/api.js
@@ -0,0 +1,170 @@
+// inisiasi library
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const mysql = require("mysql")
+
+// implementation
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Create mysql connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "pelanggaran_siswa"
+})
+
+db.connect(error => {
+    if(error){
+        console.log(error.message)
+    } else {
+        console.log("Mysql Connected")
+    }
+})
+
+// // End-Point akses data siswa
+// app.get("/siswa", (req,res) => {
+//     // Create sql query
+//     let sql = "select * from siswa"
+
+//     // run query
+//     db.query(sql, (error, result) => {
+//         let response = null
+//         if(error) {
+//             response = {
+//                 message: error.message // pesan error
+//             }
+//         } else {
+//             response = {
+//                 count: result.length, // jumlah data
+//                 siswa: result // isi data
+//             }
+//         }
+
+//         res.json(response)
+//     })
+// })
+
+// // end-point akses data siswa berdasarkan id_siswa tertentu
+// app.get("/siswa/:id", (req,res) => {
+//     let data = {
+//         id_siswa: res.params.id
+//     }
+
+//     // Create sql query
+//     let sql = "select * from siswa where ?"
+
+//     // run query
+//     db.query(sql, data, (error, result) => {
+//         let response = null
+//         if(error) {
+//             response = {
+//                 message: error.message // pesan error
+//             }
+//         } else {
+//             response = {
+//                 count: result.length, // jumlah data
+//                 siswa: result // isi data
+//             }
+//         }
+//         res.json(response) // send response
+//     })
+// })
+
+// // end-point menyimpan data
+// app.post("/siswa", (req,res) => {
+//     // prepare data
+//     let data = {
+//         nis: req.body.nis,
+//         nama_siswa: req.body.nama_siswa,
+//         kelas: req.body.kelas,
+//         poin: req.body.poin
+//     }
+
+//     // Create sql query insert
+//     let sql = "insert into siswa set ?"
+
+//     // Run query
+//     db.query(sql, data, (error,result) => {
+//         let response = null
+//         if(error) {
+//             response = {
+//                 message: error.message // pesan error
+//             }
+//         } else {
+//             response = {
+//                 message: result.affectedRows + " data inserted"
+//             }
+//         }
+//         res.json(response) // send response
+//     })
+// })
+
+// // end-point mengubah data siswa
+// app.put("/siswa", (req,res) => {
+//     // prepare data
+//     let data = [
+//         {
+//             nis: req.body.nis,
+//             nama_siswa: req.nody.nama_siswa,
+//             kelas: req.body.kelas,
+//             poin: req.body.poin
+//         },
+//         // Parameter (primary key)
+//         {
+//             id_siswa: req.body.id_siswa
+//         }
+//     ]
+
+//     // Create sql query update
+//     let sql = "update siswa set ? where ?"
+
+//     // run query
+//     db.query(sql, data, (error,result) => {
+//         let response = null
+//         if(error) {
+//             response = {
+//                 message: error.message // pesan error
+//             }
+//         } else {
+//             response = {
+//                 message: result.affectedRows + " data updated"
+//             }
+//         }
+//         res.json(response)
+//     })
+// })
+
+// // End-point menghapus data siswa berdasarkan id_siswa
+// app.delete("/siswa/:id", (req,res) => {
+//     // prepare data
+//     let data = {
+//         id_siswa: req.params.id
+//     }
+
+//     // create query sql delete
+//     let sql = "delete from siswa where ?"
+
+//     // run query
+//     db.query(sql, data, (error,result) => {
+//         let response = null
+//         if(error){
+//             response = {
+//                 message: error.message
+//             }
+//         } else {
+//             response = {
+//                 message: result.affectedRows + " data deleted"
+//             }
+//         }
+//         res.json(response)
+//     })
+// })
+
+app.listen(8000, () => {
+    console.log("running on post 8000")
+})
